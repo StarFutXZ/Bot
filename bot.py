3ff5ce29ceea52848cc80dbfe6d187e13448fbfb
StarFutXZ <andrepequeno31@gmail.com>
2026-09-11 03:40:06 +0100

    Update status emoji format in bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def enviar_ou_atualizar():
                             versao = exp.get("version", "")
                             atualizado = exp.get("updateStatus", False)
                             
-                            status_emoji = "<a:zw_GreenCircle:1547795863433248808>" if atualizado else "<a:zw_RedCircle:1547796044316811354>"
+                            status_emoji = "<:zw_GreenCircle:1547798130827726889>" if atualizado else "<:zw_RedCircle:1547798037819035658>"
                             linha = f"- {nome} | `{versao}` | {status_emoji}"
                             
                             nome_lower = nome.lower()
